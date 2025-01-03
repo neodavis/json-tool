44828939bffc839f486fd0fac9a21c855ccf0e52
dadadav1s <dendhg.rgbg@gmail.com>
2025-01-03 23:53:54 +0200

    modified firestore layer

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, query, limit, orderBy, addDoc, getDocs } from '@angular/fire/firestore';
+import { Firestore, collection, query, limit, orderBy, addDoc, getDocs, doc, getDoc, deleteDoc, updateDoc } from '@angular/fire/firestore';
 import { DocumentType } from '../interfaces/document.interface';
-import { where } from 'firebase/firestore';
+import { increment, where } from 'firebase/firestore';
 
 @Injectable({ providedIn: 'root' })
 export class FirestoreService {
@@ -9,7 +9,12 @@ export class FirestoreService {
 
   async saveDocument(type: DocumentType, content: string, userId: string) {
     const collectionRef = collection(this.firestore, type);
-    const doc = { content, timestamp: new Date(), userId };
+    const doc = { 
+      content, 
+      timestamp: new Date(), 
+      userId,
+      version: 1
+    };
     return addDoc(collectionRef, doc);
   }
 
@@ -19,6 +24,35 @@ export class FirestoreService {
       where('userId', '==', userId),
       orderBy('timestamp', 'desc'),
       limit(limitQuantity)
+    );
+    return getDocs(q);
+  }
+
+  async getJsonById(type: DocumentType, docId: string) {
+    const docRef = doc(this.firestore, type, docId);
+    return getDoc(docRef);
+  }
+
+  async updateJson(type: DocumentType, docId: string, content: string, userId: string) {
+    const docRef = doc(this.firestore, type, docId);
+    return updateDoc(docRef, {
+      content,
+      timestamp: new Date(),
+      userId,
+      version: increment(1)
+    });
+  }
+
+  async deleteJson(type: DocumentType, docId: string) {
+    const docRef = doc(this.firestore, type, docId);
+    return deleteDoc(docRef);
+  }
+
+  async getAllUserDocuments(type: DocumentType, userId: string) {
+    const q = query(
+      collection(this.firestore, type),
+      where('userId', '==', userId),
+      orderBy('timestamp', 'desc')
     );
     return getDocs(q);
   }
